62d735a2e3d6dcfc72bce5517358ea27fa5e18ba
Tomas <pedrozo.6@osu.edu>
2016-12-20 21:35:34 -0500

    Update

--- The Boss.py
+++ The Boss.py
@@ -1,7 +1,7 @@
 """
     'Counting with Bruce Springsteen' is a program that takes the whole corpus
     of the Boss's work, cleans and filters the text, and creates an analysis
-    based on the text to gather insights into themes in Springsteen's work.
+    based on the text to gather insights into themes in the music.
     
 """
 
@@ -30,17 +30,21 @@ boss_song_list = boss_song_list.tolist()
 
 #Now creating a loop that appends a list (or something) to collect all the lyrics
 i = 0
+all_lyrics = ""
 while i < len(boss_song_list):
     boss = boss_song_list[i].lower().replace(' ', '')
     punct = set(sr.punctuation)
     boss = ''.join(x for x in boss if x not in punct)
     url_songs = 'http://www.azlyrics.com/lyrics/brucespringsteen/' + boss + '.html'
-    print(url_songs)
+    #print(url_songs)
     
     # Now that we have URLs, need to get html and clean text for lyrics
+    # Below still needs work!
     html = urlopen(url_songs).read()
     new_text = bs4.BeautifulSoup(html, 'lxml')
     div = new_text.find_all('div')
     div = div[22]
+    div = div.text
+    all_lyrics += div
     i += 1
     
