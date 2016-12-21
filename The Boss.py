d0f269d3262c58965507250cf83337f203e990ec
Tomas <pedrozo.6@osu.edu>
2016-12-20 21:10:40 -0500

    Update

--- The Boss.py
+++ The Boss.py
@@ -10,6 +10,7 @@ import bs4
 import nltk
 import pandas as pd
 import re
+import string as sr
 
 #Getting and cleaning the song list
 url = urlopen('http://www.azlyrics.com/s/springsteen.html').read()
@@ -22,11 +23,24 @@ songs = []
 for song in song_list:
     songs.append(song.get_text())
 
+# Making it into a series then change it into a list
 boss_song_list = pd.Series(songs)
 boss_song_list = boss_song_list[1:]
+boss_song_list = boss_song_list.tolist()
 
 #Now creating a loop that appends a list (or something) to collect all the lyrics
-for boss in boss_song_list:
+i = 0
+while i < len(boss_song_list):
+    boss = boss_song_list[i].lower().replace(' ', '')
+    punct = set(sr.punctuation)
+    boss = ''.join(x for x in boss if x not in punct)
     url_songs = 'http://www.azlyrics.com/lyrics/brucespringsteen/' + boss + '.html'
+    print(url_songs)
+    
+    # Now that we have URLs, need to get html and clean text for lyrics
     html = urlopen(url_songs).read()
+    new_text = bs4.BeautifulSoup(html, 'lxml')
+    div = new_text.find_all('div')
+    div = div[22]
+    i += 1
     
